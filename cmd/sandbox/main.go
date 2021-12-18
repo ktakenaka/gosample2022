245b6b63f4a3f82b383dd363d4cb254e71720b91
ktakenaka <bzzs.1120@gmail.com>
2021-12-18 15:47:07 +0700

    implement db connection

--- cmd/sandbox/main.go
+++ cmd/sandbox/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/ktakenaka/gosample2022/app/models"
+	"github.com/ktakenaka/gosample2022/infra/database"
+)
 
 func main() {
 	fmt.Println("hello")
+
+	cfg := &database.Config{
+		User:     "writer",
+		Password: "writer_password",
+		Host:     "db",
+		DBName:   "gosample2022_development",
+	}
+	db, err := database.New(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	users, err := models.Users().All(ctx, db)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(users)
 }
---
+++ infra/database/mysql.go
@@ -0,0 +1,86 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	defaultOptions = map[string]string{
+		"collation":         "utf8mb4_bin",
+		"charset":           "utf8mb4",
+		"parseTime":         "true",
+		"interpolateParams": "true",
+	}
+)
+
+const (
+	defaultMaxIdleConns    = 5
+	defaultMaxOpenConns    = 10
+	defaultConnMaxLifetime = 20 * time.Second
+
+	conn = "%s:%s@tcp(%s)/%s"
+)
+
+// Config connection information
+type Config struct {
+	User            string
+	Password        string
+	Host            string
+	DBName          string
+	MaxIdleConns    int
+	MaxOpenConns    int
+	ConnMaxLifetime time.Duration
+
+	Options map[string]string
+}
+
+type DB struct {
+	*sql.DB
+}
+
+// New connect to db
+func New(cfg *Config) (*DB, error) {
+	connStr := conn
+	optionsMap := defaultOptions
+	if cfg.Options != nil {
+		for k, v := range cfg.Options {
+			optionsMap[k] = v
+		}
+	}
+
+	var optionsSlice []string
+	for k, v := range optionsMap {
+		optionsSlice = append(optionsSlice, k+"="+v)
+	}
+
+	connStr = fmt.Sprintf(conn+"?"+strings.Join(optionsSlice, "&"), cfg.User, cfg.Password, cfg.Host, cfg.DBName)
+	db, err := sql.Open("mysql", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	if cfg.MaxIdleConns == 0 {
+		db.SetMaxIdleConns(defaultMaxIdleConns)
+	} else {
+		db.SetMaxIdleConns(cfg.MaxIdleConns)
+	}
+
+	if cfg.MaxOpenConns == 0 {
+		db.SetMaxOpenConns(defaultMaxOpenConns)
+	} else {
+		db.SetMaxOpenConns(cfg.MaxOpenConns)
+	}
+
+	if cfg.ConnMaxLifetime == 0 {
+		db.SetConnMaxLifetime(defaultConnMaxLifetime)
+	} else {
+		db.SetConnMaxLifetime(cfg.ConnMaxLifetime)
+	}
+
+	return &DB{db}, nil
+}
