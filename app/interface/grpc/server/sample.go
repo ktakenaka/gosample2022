2faa4cf997430d5152a9f288effdd2c941404070
ktakenaka <bzzs.1120@gmail.com>
2022-01-03 12:47:16 +0700

    use string for the proto definition of sample.amount In the future, consider using "decimal"

--- app/interface/grpc/server/sample.go
+++ app/interface/grpc/server/sample.go
@@ -3,21 +3,33 @@ package server
 import (
 	"context"
 
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-
 	samplePb "github.com/ktakenaka/gosample2022/app/interface/grpc/protos/sample"
 	"github.com/ktakenaka/gosample2022/app/pkg/notifier"
+	"github.com/ktakenaka/gosample2022/app/pkg/ulid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // LisaSamples implements samplePb.LisaSamples
 func (s *server) ListSamples(ctx context.Context, in *samplePb.Request) (*samplePb.Response, error) {
-	_, err := s.sampleViewer.List(ctx)
+	samples, err := s.sampleViewer.List(ctx)
 
 	if err != nil {
 		// Temporary implementation
 		s.ntfr.Message(notifier.ERR, err.Error())
 		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+
+	pbSamples := make([]*samplePb.OneSample, len(samples))
+	for i := range samples {
+		pbSamples[i] = &samplePb.OneSample{
+			Id:       ulid.ULID(samples[i].ID).String(),
+			Title:    samples[i].Title,
+			Category: samples[i].Category,
+			Memo:     samples[i].Memo,
+			Date:     samples[i].Date.String(),
+			Amount:   samples[i].Amount.String(),
+		}
 	}
 	return &samplePb.Response{}, err
 }
