b1efe944a399a8853fb69e21daa5af27911bfc2a
ktakenaka <bzzs.1120@gmail.com>
2022-01-03 11:05:39 +0700

    temporary implementation for error handling

--- app/interface/grpc/server/sample.go
+++ app/interface/grpc/server/sample.go
@@ -3,6 +3,9 @@ package server
 import (
 	"context"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	samplePb "github.com/ktakenaka/gosample2022/app/interface/grpc/protos/sample"
 	"github.com/ktakenaka/gosample2022/app/pkg/notifier"
 )
@@ -10,8 +13,11 @@ import (
 // LisaSamples implements samplePb.LisaSamples
 func (s *server) ListSamples(ctx context.Context, in *samplePb.Request) (*samplePb.Response, error) {
 	_, err := s.sampleViewer.List(ctx)
-	if err == nil {
-		s.ntfr.Message(notifier.INFO, "success")
+
+	if err != nil {
+		// Temporary implementation
+		s.ntfr.Message(notifier.ERR, err.Error())
+		return nil, status.Errorf(codes.Internal, err.Error())
 	}
 	return &samplePb.Response{}, err
 }
