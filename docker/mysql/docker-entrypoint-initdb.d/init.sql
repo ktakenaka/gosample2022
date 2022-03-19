507fe2d94ea47f8cc007f1d869fbc8638a6513bc
ktakenaka <bzzs.1120@gmail.com>
2022-03-19 16:17:08 +0700

    Use txdb instead of preparing 8 dbs

--- docker/mysql/docker-entrypoint-initdb.d/init.sql
+++ docker/mysql/docker-entrypoint-initdb.d/init.sql
@@ -8,17 +8,4 @@ CREATE USER 'reader' IDENTIFIED BY 'reader_password';
 GRANT SELECT, SHOW VIEW ON `gosample2022_development`.* TO 'reader'@'%';
 
 # For testing
-CREATE DATABASE IF NOT EXISTS `gosample2022_test1` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE DATABASE IF NOT EXISTS `gosample2022_test2` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE DATABASE IF NOT EXISTS `gosample2022_test3` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE DATABASE IF NOT EXISTS `gosample2022_test4` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE DATABASE IF NOT EXISTS `gosample2022_test5` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE DATABASE IF NOT EXISTS `gosample2022_test6` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE DATABASE IF NOT EXISTS `gosample2022_test7` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE DATABASE IF NOT EXISTS `gosample2022_test8` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-
-CREATE USER 'test_writer'@'%' IDENTIFIED BY 'writer_password';
-GRANT ALL PRIVILEGES ON `gosample2022_test%`.* TO 'test_writer'@'%' WITH GRANT OPTION;  
-
-CREATE USER 'test_reader' IDENTIFIED BY 'reader_password';
-GRANT SELECT, SHOW VIEW ON `gosample2022_test%`.* TO 'test_reader'@'%';
+CREATE DATABASE IF NOT EXISTS `gosample2022_test` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
