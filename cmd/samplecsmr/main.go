5da4c147251c1c5a2b390744309aa406e0328483
ktakenaka <bzzs.1120@gmail.com>
2022-04-10 23:45:49 +0700

    Enable to log transaction

--- cmd/samplecsmr/main.go
+++ cmd/samplecsmr/main.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/google/go-cmp/cmp"
+
 	"github.com/ktakenaka/gosample2022/app/pkg/debeziumcsmr"
 	"github.com/ktakenaka/gosample2022/cmd/internal/config"
 	"github.com/ktakenaka/gosample2022/cmd/internal/kafkaclient"
@@ -28,39 +30,14 @@ func main() {
 		panic(err)
 	}
 	for msg := range pcsmer.Messages() {
-		hoge := &Hoge{}
-		if err := json.Unmarshal(msg.Value, hoge); err != nil {
+		payload := &debeziumcsmr.SamplePayload{}
+		if err := json.Unmarshal(msg.Value, payload); err != nil {
 			fmt.Println(err)
 			continue
 		}
-		fmt.Println("=== ID ===", hoge.Payload.After.Id)
-		fmt.Println("Code", hoge.Payload.After.Code)
-		fmt.Println("Category", hoge.Payload.After.Category)
-		fmt.Println("Amount", hoge.Payload.After.Amount)
-		fmt.Println("ValidFrom", hoge.Payload.After.ValidFrom)
-		fmt.Println("ValidTo", hoge.Payload.After.ValidTo)
-		fmt.Println("CreatedAt", hoge.Payload.After.CreatedAt)
-		fmt.Println("DeletedAt", hoge.Payload.After.DeletedAt)
+
+		fmt.Println("Before     ", cmp.Diff(payload.Payload.Before, &debeziumcsmr.Sample{}))
+		fmt.Println("After      ", cmp.Diff(payload.Payload.After, &debeziumcsmr.Sample{}))
+		fmt.Println("Transaction", cmp.Diff(payload.Payload.Transaction, &debeziumcsmr.Transaction{}))
 	}
-}
-
-type Hoge struct {
-	Payload Payload `json:"payload"`
-}
-
-type Payload struct {
-	After DSample `json:"after"`
-}
-
-type DSample struct {
-	Id        uint                  `json:"id"`
-	Biid      string                `json:"biid"`
-	OfficeId  string                `json:"office_id"`
-	Code      string                `json:"code"`
-	Category  string                `json:"category"`
-	Amount    debeziumcsmr.Decimal  `json:"amount"`
-	ValidFrom debeziumcsmr.Date     `json:"valid_from"`
-	ValidTo   debeziumcsmr.Date     `json:"valid_to"`
-	CreatedAt debeziumcsmr.Time     `json:"created_at"`
-	DeletedAt debeziumcsmr.NullTime `json:"deleted_at"`
 }
