43343d2e524765dd7de608596d4568b067c31d35
ktakenaka <bzzs.1120@gmail.com>
2022-05-26 23:02:10 +0700

    Upload file

--- cmd/sandbox/s3/main.go
+++ cmd/sandbox/s3/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/ktakenaka/gosample2022/cmd/internal/config"
 	infraAWS "github.com/ktakenaka/gosample2022/infra/aws"
@@ -18,7 +20,7 @@ var (
 
 func main() {
 	sess, _ := infraAWS.NewSession(&infraAWS.Config{
-		ID: cfg.AWS.ID, Secret: cfg.AWS.Secret, Region: cfg.AWS.Region, Endpoint: cfg.AWS.Endpoint,
+		ID: cfg.AWS.ID, Secret: cfg.AWS.Secret, Region: cfg.AWS.Region, Endpoint: "http://s3.localhost.localstack.cloud:4566",
 	})
 	client := infraAWS.NewS3(sess)
 
@@ -26,8 +28,21 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	for _, b := range out.Buckets {
 		log.Println(*b.Name)
 	}
+
+	f, err := os.Open("go.mod")
+	if err != nil {
+		log.Fatal(err)
+	}
+	out2, err := client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String("example"),
+		Body:   f,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(out2.ETag)
 }
